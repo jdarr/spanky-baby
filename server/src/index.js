98bf4aa323b3bbcde21e6354d92a0d807428bb5c
Jared Darr <jared.darr@gmail.com>
2026-04-24 16:28:11 -0700

    Serve static from server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,13 @@ import http from "http";
 import { Server as SocketIOServer } from "socket.io";
 import { makeRedisClient } from "./redis.js";
 
+import { createServer } from 'node:http';
+import { readFile } from 'node:fs';
+import { join, extname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('../', import.meta.url));
+
 const PORT = Number(process.env.PORT || 3001);
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 const REDIS_KEY = process.env.REDIS_KEY || "counter:value";
@@ -27,14 +34,52 @@ async function incrCounter() {
   return await redis.incr(REDIS_KEY);
 }
 
-const httpServer = http.createServer((req, res) => {
-  if (req.url === "/health") {
+const httpServer = createServer((req, res) => {
+
+	// check heaalth of the server
+	if (req.url === "/health") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify({ ok: true }));
     return;
   }
-  res.writeHead(404);
-  res.end();
+
+	// load static files if they exist	
+	let filePath = join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+	
+	const mimeTypes = {
+			'.html': 'text/html',
+			'.js': 'text/javascript',
+			'.css': 'text/css',
+			'.json': 'application/json',
+			'.png': 'image/png',
+			'.svg': 'image/svg+xml',
+			'.ico': 'image/x-icon',
+			'.wav': 'audio/wav',
+			'.mp3': 'audio/mpeg',
+			'.woff': 'font/woff',
+			'.woff2': 'font/woff2',
+			'.ttf': 'font/ttf',
+			'.otf': 'font/otf',
+			'.wasm': 'application/wasm',
+	};
+	
+	const contentType = mimeTypes[extname(filePath)] || 'application/octet-stream';
+
+	readFile(filePath, (error, content) => {
+		if (error) {
+				if (error.code === 'ENOENT') {
+						res.writeHead(404, { 'Content-Type': 'text/html' });
+						res.end('<h1>404 Not Found</h1>', 'utf-8');
+				} else {
+						res.writeHead(500);
+						res.end(`Server Error: ${error.code}`);
+				}
+		} else {
+				res.writeHead(200, { 'Content-Type': contentType });
+				res.end(content, 'utf-8');
+		}
+  });
+
 });
 
 const io = new SocketIOServer(httpServer, {
